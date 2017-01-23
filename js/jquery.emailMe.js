dba000d7acd25b9260e5b6589d18d7c6a8120d70
Igor Likhachev <igor.likhachev@gmail.com>
2017-01-23 14:00:51 +0300

    some cleanup

--- js/jquery.emailMe.js
+++ js/jquery.emailMe.js
@@ -1,4 +1,4 @@
-;jQuery(document).ready(function($) {
+;jQuery(document).ready( function($) {
 	var ins = ['c','o','m', 'i','g','o','r','l','i','k','h','a','c','h','e','v','g','m','a','i','l'];
 	var ndx = [ 21, 22, 23, 0,  1,  2,  3,  5,  6,  7,  8,  9, 10, 11, 12, 13, 15, 16, 17, 18, 19];
 	var outs = new Array();
